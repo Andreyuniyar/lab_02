1b2ec0473966de98960a103787622070f9eec9d1
Andreyuniyar <147002682+Andreyuniyar@users.noreply.github.com>
2023-10-05 01:20:27 +0300

    Update main.java

--- main.java
+++ main.java
@@ -1,1 +1,79 @@
+//public class Main {
+//    public static void main(String[] args) {
+//        int mas[] = {121, 201, 555, 456, 425, 666, 502, 800, 20, 10, 15, 13, 16, 850, 454, 232, 150, 17, 50};
+//        int count = 0;
+//        int summ = 0;
+//        for (int i = 0; i <= 19; i++){
+//            if ((mas[i] / 100) != 0){
+//                if ((mas[i] / 100) != 0){
+//                    count++;
+//                    summ += mas[i];
+//                }
+//            }
+//        }
+//        System.out.println("Кол-во: " + count);
+//        System.out.println("Сумма: " + summ);
+//    }
+//}
+
+//public class Main {
+//    public static void main(String[] args) {
+//        int mas[] = {121, 201, 555, 456, 425, 666, 502, 800, 20, 10, 15, 13, 16, 850, 454, 232, 150, 17, 50};
+//        int max_nam = 0;
+//        for (int i = 0; i <= 19; i++){
+//            if ((mas[i] % 2 == 0) && (mas[i] > max_nam)){
+//                max_nam = mas[i];
+//            }
+//        }
+//        System.out.println("Наибольшее число: " + max_nam);
+//    }
+//}
+
+//import java.util.Arrays;
+//import java.util.Comparator;
+//import java.util.Collections;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        int mas[] = {121, 201, 555, 456, 425, 666, 502, 800, 20, 10, 15, 13, 16, 850, 454, 232, 150, 17, 50};
+//        Integer mas2[] = new Integer[20];
+//        Arrays.fill(mas2, 0);
+//        for (int i = 0; i <= 19; i++){
+//            if (mas[i] % 10 == 3){
+//                mas[2] = mas[i];
+//            }
+//        }
+//        Arrays.sort(mas2, Collections.reverseOrder());
+//        System.out.println(Arrays.toString(mas2));
+//    }
+//}
+
+//import java.util.Arrays;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        String mas[] = new String[1000000];
+//        Arrays.fill(mas, 'String');
+//        String mas2[] = new String[];
+//        String mas3[] = new String[];
+//        long StartTime = System.nanoTime();
+//        for (int i = 0; i < 1000000; i++){
+//            mas2[i] == mas1[i]
+//        }
+//        long EndTime = System.nanoTime();
+//        long Duration = StartTime - EndTime;
+//        System.out.println("For: " + Duration);
+//        StartTime = System.nanoTime();
+//        System.arraycopy(mas, 0, mas3, 0, 1000000);
+//        EndTime = System.nanoTime();
+//        Duration = StartTime - EndTime;
+//        System.out.println("System: " + Duration);
+//        StartTime = System.nanoTime();
+//        String[] mas4 = Arrays.copyOf(mas, 1000000);
+//        EndTime = System.nanoTime();
+//        Duration = StartTime - EndTime;
+//        System.out.println("Arrays: " + Duration);
+//    }
+//}
+
 
